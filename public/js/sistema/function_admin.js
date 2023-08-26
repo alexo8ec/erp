8dfbdd7a8d88a2695401a12a11ff70d0c618655e
alexo8ec <alexo8ec@hotmail.com>
2023-08-25 22:11:20 -0500

    revision login 25/08/2023

--- public/js/sistema/function_admin.js
+++ public/js/sistema/function_admin.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
         return json;
     });
 
-    console.log(monthCharges.responseJSON[0]);
+    console.log(monthCharges.responseJSON);
     return;
 
     let arrayData2 = [];
