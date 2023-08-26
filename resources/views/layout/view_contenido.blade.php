5141aa6be8037b3f327c55aee9b1a2b7678e170b
alexo8ec <alexo8ec@hotmail.com>
2023-08-25 19:57:14 -0500

    revision login 25/08/2023

--- resources/views/layout/view_contenido.blade.php
+++ resources/views/layout/view_contenido.blade.php
@@ -69,7 +69,7 @@
                 <div class="ibox-title">
                     <h5>Orders</h5>
                 </div>
-                <input id="total_ventas_anual" value="{{json_encode(config('data.total_ventas_anual'))}}" />
+                <input id="total_ventas_anual" value="<?= json_encode(config('data.total_ventas_anual')); ?>" />
                 <div class="ibox-content">
                     <div class="row">
                         <div class="col-lg-9">
