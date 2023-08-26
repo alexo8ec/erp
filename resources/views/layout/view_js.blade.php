cd87f52eb918c7751b0f4f709593ed8551838207
alexo8ec <alexo8ec@hotmail.com>
2023-08-26 18:10:10 -0500

    revision login 25/08/2023

--- resources/views/layout/view_js.blade.php
+++ resources/views/layout/view_js.blade.php
@@ -1,3 +1,33 @@
+<script src="{{asset('public/js/popper.min.js')}}"></script>
+<script src="{{asset('public/js/bootstrap.js')}}"></script>
+<script src="{{asset('public/js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+<script src="{{asset('public/js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.spline.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.resize.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.pie.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.symbol.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/curvedLines.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.time.js')}}"></script>
+<script src="{{asset('public/js/plugins/peity/jquery.peity.min.js')}}"></script>
+<script src="{{asset('public/js/demo/peity-demo.js')}}"></script>
+<script src="{{asset('public/js/inspinia.js')}}"></script>
+<script src="{{asset('public/js/plugins/pace/pace.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<script src="{{asset('public/js/plugins/easypiechart/jquery.easypiechart.js')}}"></script>
+<script src="{{asset('public/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{asset('public/js/demo/sparkline-demo.js')}}"></script>
+<script src="{{asset('public/js/plugins/chartJs/Chart.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
+<script src="{{ url('/') }}/public/js/plugins/select2/select2.full.min.js"></script>
+<?= config('data.table') == true ? '<script src="public/js/plugins/dataTables/datatables.min.js"></script>' : ''; ?>
+<script src="{{asset('public/js/sistema/functions.js')}}"></script>
+<script src="{{ url('/') }}/public/js/sistema/function_{{config('data.controlador')}}.js"></script>
+
+
 <!--<script src="{{ url('/') }}/public/js/popper.min.js"></script>
 <script src="{{ url('/') }}/public/js/bootstrap.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/metisMenu/jquery.metisMenu.js"></script>
