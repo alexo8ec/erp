4536f3ac1a8d5b49dea8b2937868019da95fe8f8
alexo8ec <alexo8ec@hotmail.com>
2023-08-25 21:40:30 -0500

    revision login 25/08/2023

--- resources/views/layout/view_contenido.blade.php
+++ resources/views/layout/view_contenido.blade.php
@@ -69,6 +69,7 @@
                 <div class="ibox-title">
                     <h5>Orders</h5>
                 </div>
+                <input type="hidden" id="cobros_mensuales" value="{{json_encode(config('data.cobros_mensuales'))}}" />
                 <div class="ibox-content">
                     <div class="row">
                         <div class="col-lg-9">
