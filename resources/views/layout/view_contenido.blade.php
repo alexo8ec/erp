264e8c860c2b99089c48f8d54b03d2905f9a064b
alexo8ec <alexo8ec@hotmail.com>
2023-10-14 00:18:20 -0500

    Actualizacion 13-10-2023

--- resources/views/layout/view_contenido.blade.php
+++ resources/views/layout/view_contenido.blade.php
@@ -415,30 +415,14 @@
     </div>
 </div>
 
-<!-- Peity -->
-<script src="js/plugins/peity/jquery.peity.min.js"></script>
-<script src="js/demo/peity-demo.js"></script>
 
-<!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-
-<!-- jQuery UI -->
-<script src="js/plugins/jquery-ui/jquery-ui.min.js"></script>
-
-<!-- Jvectormap -->
-<script src="js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
-<script src="js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-
-<!-- EayPIE -->
-<script src="js/plugins/easypiechart/jquery.easypiechart.js"></script>
-
-<!-- Sparkline -->
-<script src="js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
-<!-- Sparkline demo data  -->
-<script src="js/demo/sparkline-demo.js"></script>
-
+<script src="{{asset('public/js/plugins/flot/jquery.flot.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.spline.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.resize.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.pie.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.symbol.js')}}"></script>
+<script src="{{asset('public/js/plugins/flot/jquery.flot.time.js')}}"></script>
 <script>
     $(document).ready(function() {
         $('.chart').easyPieChart({
