b377ddc334e07cbd275faf592a53f4116a5020ce
alexo8ec <alexo8ec@hotmail.com>
2023-10-13 23:57:44 -0500

    Actualizacion 13-10-2023

--- resources/views/layout/view_js.blade.php
+++ resources/views/layout/view_js.blade.php
@@ -2,22 +2,35 @@
 <script src="{{ url('/') }}/public/js/bootstrap.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/metisMenu/jquery.metisMenu.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
 <script src="{{asset('public/js/plugins/flot/jquery.flot.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.spline.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.resize.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.pie.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.symbol.js')}}"></script>
-<script src="{{asset('public/js/plugins/flot/curvedLines.js')}}"></script>
 <script src="{{asset('public/js/plugins/flot/jquery.flot.time.js')}}"></script>
+
 <script src="{{asset('public/js/plugins/peity/jquery.peity.min.js')}}"></script>
-<script src="{{ url('/') }}/public/js/plugins/chartJs/Chart.min.js"></script>
+<script src="{{asset('public/js/demo/peity-demo.js')}}"></script>
+
+<script src="{{ url('/') }}/public/js/inspinia.js"></script>
+<script src="{{ url('/') }}/public/js/plugins/pace/pace.min.js"></script>
+
+<script src="{{ url('/') }}/public/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+
+<script src="{{asset('public/js/plugins/easypiechart/jquery.easypiechart.js')}}"></script>
+<script src="{{asset('public/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+
+
 <?php
 if (config('data.controlador') != null && config('data.controlador') == 'admin') {
 }
 ?>
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/select2/select2.full.min.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/toastr/toastr.min.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/datapicker/bootstrap-datepicker.js"></script>
@@ -26,16 +39,10 @@ if (config('data.controlador') != null && config('data.controlador') == 'admin')
 <script src="{{ url('/') }}/public/js/plugins/ladda/ladda.min.js"></script>
 <script src="{{ url('/') }}/public/js/plugins/ladda/ladda.jquery.min.js"></script>
 
-<script src="{{ url('/') }}/public/js/plugins/fullcalendar/moment.min.js"></script>
-<script src="{{asset('public/js/demo/peity-demo.js')}}"></script>
-<script src="{{ url('/') }}/public/js/inspinia.js"></script>
-<script src="{{ url('/') }}/public/js/plugins/pace/pace.min.js"></script>
-<script src="{{ url('/') }}/public/js/plugins/jquery-ui/jquery-ui.min.js"></script>
-<script src="{{ url('/') }}/public/js/plugins/iCheck/icheck.min.js"></script>
-<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<script src="{{asset('public/js/plugins/easypiechart/jquery.easypiechart.js')}}"></script>
-<script src="{{asset('public/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{ url('/') }}/public/js/plugins/fullcalendar/moment.min.js"></script> -->
+
+
+
 
 <?php
 if (config('data.calendario') != '' && config('data.calendario') == true) {
