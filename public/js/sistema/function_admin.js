0d792aceef39cac264166de70f122b81eb910f53
alexo8ec <alexo8ec@hotmail.com>
2023-10-13 22:37:14 -0500

    Actualizacion 13-10-2023

--- public/js/sistema/function_admin.js
+++ public/js/sistema/function_admin.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    console.log($('#ventas_mensuales').val());
+    let monthSales = JSON.parse($('#ventas_mensuales').val());
+    console.log(monthSales);
     try {
         $('#tablaPlanCuentas').DataTable({
             language: {
